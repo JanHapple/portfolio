d68c1303fa74e8e3232ab7b2647dd620655ef5cc
JanHapple <janhapple@outlook.de>
2021-11-12 23:50:40 +0100

    added responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from 'react';
 import "../styles/_variables.scss";
 import "../styles/_reset.scss";
-import "../styles/Navbar.scss";
+// import "../styles/Navbar.scss";
+import classes from "../styles/Navbar.scss";
+import { CgMenuGridO } from "react-icons/cg";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+
     return(
-        <header className={classes.header}>
-            <div className={classes.headerContent}>
-                <h2>Navbar</h2>
+        <header className="header">
+            <div className="headerContent">
+                <h2 className="headerContentLogo">Navbar</h2>
+            
+                <nav className={`headerContent ${menuOpen ? `isMenu` : ""}`}>
+                    <ul className="navList">
+                        <li>
+                            <a href="/">About</a>
+                        </li>
+                        <li>
+                            <a href="/">Skills</a>
+                        </li>
+                        <li>
+                            <a href="/">Work</a>
+                        </li>
+                    </ul>
+                    <button>Contact</button>
+                </nav>
+                <div className={"headerContentToggle"}>
+                    {!menuOpen ? (<CgMenuGridO onClick={menuToggleHandler} />) : (<AiOutlineClose onClick={menuToggleHandler}/>)}
+                </div>
             </div>
-            <nav className={classes.headerContentNav}>
-                <ul>
-                    <li>
-                        <a href="/">About</a>
-                    </li>
-                    <li>
-                        <a href="/">Skills</a>
-                    </li>
-                    <li>
-                        <a href="/">Work</a>
-                    </li>
-                    <li>
-                        <a href="/">Contact</a>
-                    </li>
-                </ul>
-            </nav>
         </header>
     )
     
