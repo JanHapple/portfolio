5365f6e697cb294d1b1ebd9e1c9d98786ed3d31e
Janowitz <janhapple@outlook.de>
2021-11-19 19:49:49 +0100

    experimenting with css shapes in header

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/Home.scss";
 import Aos from "aos";
 import BrandSign from "../components/BrandSign";
+import Divider from "../components/Divider"
 
 Aos.init();
 
@@ -9,13 +10,17 @@ const Home = () => {
     return(
     <main>
         <section className="brand-container">
+        <div className="rectangle">Rectagle</div>
             <BrandSign className="brand typewriter-text"/>
             {/* <div className="triangle-left"></div>
             <div className="triangle-right"></div>             */}
         </section>
 
         <section className="about">
+            <Divider />
+            {/* <div className="rectangle">Rectagle</div> */}
             <h1>Welcome to my Website</h1>
+            <p>jkljkfd jfkdls fjsakl fsjdkl </p>
         </section>
     </main>
 
